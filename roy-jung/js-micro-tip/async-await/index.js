9fc449627fece1a428b8da653c777fcec087176c
DataCodeLiteracy <dataliteracy@icloud.com>
2023-09-23 14:05:40 +0900

    study: 비동기 처리는 무조건 async-await?
    2023.09.23

---
+++ roy-jung/js-micro-tip/async-await/index.js
@@ -0,0 +1,27 @@
+function renderProduct(product) {
+  const resultElem = document.querySelector('#result')
+  resultElem.replaceChildren()
+  resultElem.insertAdjacentHTML(
+    'beforeend',
+    `
+      <p>${product.name}</p>
+      <p>${product.timestamp}</p>
+      <p>${product.message}</p>
+    `
+  )
+}
+
+document.querySelector('#selector')?.addEventListener('change', handleChange)
+
+async function getProduct(id) {
+  const res = await fetch(`https://api.escuelajs.co/api/v1/products/${id}`)
+  const json = await res.json()
+  return json
+}
+
+async function handleChange(e) {
+  const id = e.target.value
+  const product = await getProduct(id)
+
+  renderProduct(product)
+}
