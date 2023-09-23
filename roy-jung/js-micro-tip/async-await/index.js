a2010199ae062d8700f8f8b75ce8d7d958ee9e86
DataCodeLiteracy <dataliteracy@icloud.com>
2023-09-23 14:58:29 +0900

    study: fetch 함수 글로벌 함수로 추출하기
    * fetch 함수 글로벌 함수로 추출하기
      - vscode 우클릭 리팩토링 기능으로 추출
    
    2023.09.23

--- roy-jung/js-micro-tip/async-await/index.js
+++ roy-jung/js-micro-tip/async-await/index.js
@@ -13,9 +13,14 @@ function renderProduct(product) {
 
 document.querySelector('#selector')?.addEventListener('change', handleChange)
 
-async function getProduct(id) {
+async function fetchProduct(id) {
   const res = await fetch(`https://api.escuelajs.co/api/v1/products/${id}`)
   const json = await res.json()
+  return json
+}
+
+async function getProduct(id) {
+  const json = fetchProduct(id)
   return json
 }
 
