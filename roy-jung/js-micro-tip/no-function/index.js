009fdb2d7ea289603dc0433e1ee6e68c6ae1a119
DataCodeLiteracy <dataliteracy@icloud.com>
2023-09-27 21:25:37 +0900

    study: 일반 함수를 사용할 떄 화살표 함수를 사용하라.
    2023.09.27

--- roy-jung/js-micro-tip/no-function/index.js
+++ roy-jung/js-micro-tip/no-function/index.js
@@ -1,35 +1,13 @@
-// 생성자 함수로 사용
-function Foo(...args) {
-  if (this !== window) this.args = args
-  else return args
-}
-Foo.prototype.getArgs = function () {
-  return this.args
+// 일반 함수
+function foo(...args) {
+  console.log(args)
 }
 
-const foo = new Foo(1, 2)
+// 화살표 함수
 
-for (let prop in foo) {
-  if (foo.hasOwnProperty(prop)) console.log(prop)
+const bar = (...args) => {
+  console.log(args)
 }
 
-// console.dir(foo)
-
-class Bar {
-  constructor(...args) {
-    if (this !== window) this.args = args
-    else return args
-  }
-
-  getArgs() {
-    return this.args
-  }
-}
-
-const bar = new Bar(3, 4)
-
-for (let prop in bar) {
-  console.log(prop)
-}
-
-// console.dir(bar)
+console.dir(foo)
+console.dir(bar)
