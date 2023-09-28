411c8d04dc3f782325e3d3fe1afe7a4b2cd7ae94
DataCodeLiteracy <dataliteracy@icloud.com>
2023-09-28 11:20:43 +0900

    study: click-code 초기 코드
    2023.09.28

--- roy-jung/js-micro-tip/if-else/click-code.js
+++ roy-jung/js-micro-tip/if-else/click-code.js
@@ -1,17 +1,21 @@
-const pageCodes = {
-  city: 'ci',
-  country: 'co',
-  sight: 'si'
+const getClickCode = (page, keyword) => {
+  let code
+  if (page === 'city') {
+    if (keyword === 'title') code = 'cit'
+    else if (keyword === 'more') code = 'cim'
+    else if (keyword === 'tab') code = 'cib'
+  } else if (page === 'country') {
+    if (keyword === 'title') code = 'cot'
+    else if (keyword === 'more') code = 'com'
+    else if (keyword === 'tab') code = 'cob'
+  } else if (page === 'sight') {
+    if (keyword === 'title') code = 'sit'
+    else if (keyword === 'more') code = 'sim'
+    else if (keyword === 'tab') code = 'sib'
+  }
+  return code
 }
 
-const keywordCodes = {
-  title: 't',
-  more: 'm',
-  tab: 'b'
-}
-
-const getClickCodes = (page, keyword) => pageCodes[page] + keywordCodes[keyword]
-
-console.log(getClickCodes('city', 'tab')) // cib
-console.log(getClickCodes('country', 'title')) // cot
-console.log(getClickCodes('sight', 'more')) // sim
+console.log(getClickCode('city', 'tab')) // cib
+console.log(getClickCode('country', 'title')) // cot
+console.log(getClickCode('sight', 'more')) // sim
